364c5e574c74e138e5c51ceb0a0fc33869bf72bc
NEUAI <im@aizhengpeng.cn>
2020-08-02 16:16:30 +0800

    Update files

--- blockchain/application/fedcoin.py
+++ blockchain/application/fedcoin.py
@@ -2,7 +2,8 @@ from blockchain.network.message import JoinMessage, QuitMessage
 from blockchain.consensus.posap import PoSapBlock, PoSapMessageHandler, TaskMessage, BlockMessage
 from blockchain.application.application import Application
 from blockchain.util.settings import *
-import math
+import numpy as np
+import random
 import tensorflow as tf
 import time
 
@@ -11,6 +12,7 @@ class FedCoin(Application):
     def __init__(self):
         super(FedCoin, self).__init__()
         self.msg_handler = PoSapMessageHandler(self)
+        self.app_vars['t'] = 0
         self.app_vars['ave_s'] = [0] * K
         self.app_vars['local_blk'] = PoSapBlock(0, '\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
                                                    '\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00')
@@ -52,39 +54,56 @@ class FedCoin(Application):
 
         (x_train, y_train), (x_test, y_test) = mnist.load_data()
         x_train, x_test = x_train / 255.0, x_test / 255.0
-        # x_train = x_train.reshape((60000, 28, 28, 1))
-        # x_test = x_test.reshape((10000, 28, 28, 1))
+        x_train = x_train.reshape(-1, 28 * 28)
+        x_test = x_test.reshape(-1, 28 * 28)
 
-        local_size = math.floor(len(x_train) / K)
+        a = [[], [], [], [], [], [], [], [], [], []]
+        b = [[], [], [], [], [], [], [], [], [], []]
 
-        for i in range(K):
-            model = tf.keras.models.Sequential([
-                tf.keras.layers.Flatten(input_shape=(28, 28)),
-                tf.keras.layers.Dense(128, activation='relu'),
-                tf.keras.layers.Dropout(0.2),
-                tf.keras.layers.Dense(10, activation='softmax')
-                # tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
-                # tf.keras.layers.MaxPooling2D((2, 2)),
-                # tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-                # tf.keras.layers.MaxPooling2D((2, 2)),
-                # tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
-                # tf.keras.layers.Flatten(),
-                # tf.keras.layers.Dense(64, activation='relu'),
-                # tf.keras.layers.Dense(10, activation='softmax')
-            ])
+        for i in range(len(y_train)):
+            a[y_train[i]].append(x_train[i])
+            b[y_train[i]].append(y_train[i])
 
-            model.compile(optimizer='adam',
-                          loss='sparse_categorical_crossentropy',
-                          metrics=['accuracy'])
+        model = tf.keras.models.Sequential([
+            tf.keras.layers.Dense(512, activation='tanh', input_shape=(784,)),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(256, activation='tanh'),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(128, activation='tanh'),
+            tf.keras.layers.Dropout(0.2),
+            tf.keras.layers.Dense(10, activation='softmax')
+        ])
 
-            model.fit(x_train[i * local_size:(i + 1) * local_size],
-                      y_train[i * local_size:(i + 1) * local_size],
-                      epochs=5)
+        model.compile(
+            loss=tf.keras.losses.SparseCategoricalCrossentropy(),
+            optimizer=tf.keras.optimizers.SGD(learning_rate=0.02),
+            metrics=[tf.keras.metrics.SparseCategoricalAccuracy()])
 
-            model.evaluate(x_test, y_test, verbose=2)
+        init_weights = model.get_weights()
 
-            model.save('save_model/model_' + str(i + 1) + '.h5')
+        for i in range(100):
+            current_dataset_x = a[i // 10:]
+            current_dataset_y = b[i // 10:]
 
-            del model
-            tf.keras.backend.clear_session()
+            current_dataset_xx = []
+            current_dataset_yy = []
+
+            for item in current_dataset_x:
+                for _ in item:
+                    current_dataset_xx.append(_)
+
+            for item in current_dataset_y:
+                for _ in item:
+                    current_dataset_yy.append(_)
+
+            xxyy = list(zip(current_dataset_xx, current_dataset_yy))
+            random.shuffle(xxyy)
+
+            xxyy = random.sample(xxyy, 600)
+            current_dataset_xx, current_dataset_yy = zip(*xxyy)
+
+            model.fit(np.asarray(current_dataset_xx), np.asarray(current_dataset_yy), epochs=50,
+                      validation_data=(x_test, y_test))
+            model.save('save_model/' + str(i + 1) + '.h5')
+            model.set_weights(init_weights)
         return
--- blockchain/util/settings.py
+++ blockchain/util/settings.py
@@ -1,5 +1,5 @@
 # FedCoin
-K = 20
+K = 5
 D = 0.5
 PRICE = 1000.0
 RUNTIME = 30.0
